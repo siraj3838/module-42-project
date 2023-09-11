8a07c1b0d8b2fb533b615e276575a5e38b5d7ca9
Sirajul Islam <ayasabegum134@gmail.com>
2023-09-11 22:07:21 +0600

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+
+const Header = () => {
+    return (
+        <div>
+            <h1 className='text-5xl text-center text-[#0b250d86] mt-6 font-semibold'>Module 42 Project Knowledge Cafe</h1>
+        </div>
+    );
+};
+
+export default Header;
